078488f001332a1132aefb82269abd4b3e490901
Ryan Pluss <r.pluss52@gmail.com>
2019-05-01 21:05:56 -0400

    first v2 commit to master

--- scripts/main.js
+++ scripts/main.js
@@ -1,69 +1,40 @@
-const addProfileBtn = document.getElementById('add-profile');
-const profileFields = [
-    {
-        classes: ['wname'],
-        name: 'NAME'
-    },
-    {
-        classes: ['wstat'],
-        name: 'M'
-    },
-    {
-        classes: ['wstat'],
-        name: 'WS'
-    },
-    {
-        classes: ['wstat'],
-        name: 'BS'
-    },
-    {
-        classes: ['wstat'],
-        name: 'S'
-    },
-    {
-        classes: ['wstat'],
-        name: 'T'
-    },
-    {
-        classes: ['wstat'],
-        name: 'W'
-    },
-    {
-        classes: ['wstat'],
-        name: 'A'
-    },
-    {
-        classes: ['wstat'],
-        name: 'Ld'
-    },
-    {
-        classes: ['wstat'],
-        name: 'Sv'
-    },
+const battlefieldRoles = [
+    'dedicated_transport',
+    'elite',
+    'fast_attack',
+    'flyer',
+    'fortification',
+    'heavy_support',
+    'hq',
+    'lord_of_war',
+    'troop'
 ];
-function addProfile(ev){
+
+function changeBattlefieldRole(ev){
     console.log(ev);
-    let tbody = ev.target.parentNode.parentNode.children[1].children[1];
-    tbody.appendChild(makeProfileRow());
-}
-
-function makeProfileRow(){
-    let row = document.createElement('tr');
-    for(let fld of profileFields){
-        row.appendChild(makeProfileCell(fld.classes))
+    let currentClass;
+    let currentIndex;
+    for(let cls of ev.target.classList){
+        if(battlefieldRoles.indexOf(cls) > -1){
+            console.log('blah');
+            currentIndex = battlefieldRoles.indexOf(cls);
+            currentClass = battlefieldRoles[currentIndex];
+            console.log([currentIndex, currentClass]);
+            break;
+        }
     }
-    return row;
-}
-
-function makeProfileCell(classList){
-    let cell = document.createElement('td');
-    for(let cls of classList){
-        cell.classList.add(cls);
+    console.log(currentClass);
+    if(currentClass !== undefined){
+        ev.target.classList.remove(currentClass);
+        if(currentIndex === (battlefieldRoles.length - 1)){
+            ev.target.classList.add(battlefieldRoles[0]);
+        }else{
+            ev.target.classList.add(battlefieldRoles[currentIndex + 1]);
+        }
     }
-    let textarea = document.createElement('textarea');
-    textarea.setAttribute('rows', '1');
-    cell.appendChild(textarea);
-    return cell;
 }
 
-addProfileBtn.addEventListener('click', addProfile, {passive: true});
+let bfRoles = document.getElementsByClassName('battlefield-role');
+for(let el of bfRoles){
+    el.addEventListener('click', changeBattlefieldRole, {passive: true})
+}
