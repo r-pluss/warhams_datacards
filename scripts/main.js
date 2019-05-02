221b9135c4e1067285dee0d457fc5eacd40abdec
Ryan Pluss <r.pluss52@gmail.com>
2019-05-02 12:23:41 -0400

    profile additions and removal support

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,54 @@ const battlefieldRoles = [
     'lord_of_war',
     'troop'
 ];
+const profileFields = [
+    {
+        classes: ['wname'],
+        name: 'NAME',
+        placeholder: 'Enter Model Name'
+    },
+    {
+        classes: ['wstat'],
+        name: 'M'
+    },
+    {
+        classes: ['wstat'],
+        name: 'WS'
+    },
+    {
+        classes: ['wstat'],
+        name: 'BS'
+    },
+    {
+        classes: ['wstat'],
+        name: 'S'
+    },
+    {
+        classes: ['wstat'],
+        name: 'T'
+    },
+    {
+        classes: ['wstat'],
+        name: 'W'
+    },
+    {
+        classes: ['wstat'],
+        name: 'A'
+    },
+    {
+        classes: ['wstat'],
+        name: 'Ld'
+    },
+    {
+        classes: ['wstat'],
+        name: 'Sv'
+    },
+];
+
+function addProfile(ev){
+    let tbody = ev.target.parentNode.children[0].children[1];
+    tbody.appendChild(makeProfileRow());
+}
 
 function changeBattlefieldRole(ev){
     console.log(ev);
@@ -34,7 +82,42 @@ function changeBattlefieldRole(ev){
     }
 }
 
+function makeProfileRow(){
+    let row = document.createElement('tr');
+    for(let fld of profileFields){
+        row.appendChild(makeProfileCell(fld.classes, fld.hasOwnProperty('placeholder') ? fld.placeholder : undefined))
+    }
+    let removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-profile');
+    removeBtn.addEventListener('click', removeProfile, {passive: true});
+    row.appendChild(removeBtn);
+    return row;
+}
+
+function makeProfileCell(classList, placeholder){
+    let cell = document.createElement('td');
+    for(let cls of classList){
+        cell.classList.add(cls);
+    }
+    let textarea = document.createElement('textarea');
+    textarea.setAttribute('rows', '1');
+    if(placeholder !== undefined){
+        textarea.setAttribute('placeholder', placeholder);
+    }
+    cell.appendChild(textarea);
+    return cell;
+}
+
+function removeProfile(ev){
+    let tr = ev.target.parentNode;
+    tr.parentNode.removeChild(tr);
+}
+
 let bfRoles = document.getElementsByClassName('battlefield-role');
 for(let el of bfRoles){
     el.addEventListener('click', changeBattlefieldRole, {passive: true})
+}
+let addProfileBtns = document.getElementsByClassName('add-profile');
+for(let el of addProfileBtns){
+    el.addEventListener('click', addProfile, {passive: true});
 }
