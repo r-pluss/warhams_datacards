d3c4926c63c404d7af849d46e945c4c4daa4e911
Ryan Pluss <r.pluss52@gmail.com>
2019-05-17 13:54:24 -0400

    fixed missing class selector typo in extractWargearData

--- scripts/grid-compliant.js
+++ scripts/grid-compliant.js
@@ -282,7 +282,7 @@ function extractProfileData(sectionContent, data){
 
 function extractWargearData(sectionContent, data){
     let wargearList = sectionContent.querySelector('ul');
-    for(let li of wargearList.querySelectorAll('wargear-item')){
+    for(let li of wargearList.querySelectorAll('.wargear-item')){
         data.wargear.push(
             li.firstElementChild.textContent.length > 0 ?
                 li.firstElementChild.textContent : undefined
